ce78d9aacfaa910e013934bacad886bcfa6ab9fb
kaylee-k <kaylee.kihm@gmail.com>
2024-08-31 23:36:53 -0600

    Feat: Implement NoteComponent and TodoComponent using BaseComponent

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import { PageComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/image.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 
 class App {
   private readonly page: PageComponent;
@@ -12,6 +14,12 @@ class App {
       'https://picsum.photos/600/300'
     );
     image.attachTo(appRoot, 'beforeend');
+
+    const note = new NoteComponent('Note Title', 'Note Body');
+    note.attachTo(appRoot, 'beforeend');
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item');
+    todo.attachTo(appRoot, 'beforeend');
   }
 }
 
