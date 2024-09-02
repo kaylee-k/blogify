6bbb7ed0ac7d866e8c3995ac0ad5fe22a0231796
kaylee-k <kaylee.kihm@gmail.com>
2024-09-01 23:24:12 -0600

    Feat: Add dialog functionality for image input and style dialog component

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
 import { Component } from './components/component.js';
+import { InputDialog } from './components/dialog/dialog.js';
 
 class App {
   private readonly page: Component & Composable;
@@ -35,6 +36,22 @@ class App {
 
     const todo = new TodoComponent('Todo Title', 'Todo Item');
     this.page.addChild(todo);
+
+    const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+
+    imageBtn.addEventListener('click', () => {
+      const dialog = new InputDialog();
+
+      dialog.setOnCloseListener(() => {
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.setOnSubmitListener(() => {
+        dialog.removeFrom(document.body);
+      });
+
+      dialog.attachTo(document.body);
+    });
   }
 }
 
