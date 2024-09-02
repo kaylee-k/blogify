0ec88e2e5e0e1491ea67d5b0e486d9fdaf452ead
kaylee-k <kaylee.kihm@gmail.com>
2024-09-02 10:44:46 -0600

    Refactor: Implement 'MediaData' and 'TextData' interfaces for improved type safety and data handling in 'InputDialog', 'MediaSectionInput', and 'TextSectionInput'

--- src/components/dialog/input/media-input.ts
+++ src/components/dialog/input/media-input.ts
@@ -1,6 +1,10 @@
 import { BaseComponent } from '../../component.js';
+import { MediaData } from '../dialog.js';
 
-export class MediaSectionInput extends BaseComponent<HTMLElement> {
+export class MediaSectionInput
+  extends BaseComponent<HTMLElement>
+  implements MediaData
+{
   constructor() {
     super(`<div>
                     <div class="form__container">
@@ -13,6 +17,7 @@ export class MediaSectionInput extends BaseComponent<HTMLElement> {
                     </div>
                 </div>`);
   }
+
   get title(): string {
     const element = this.element.querySelector('#title')! as HTMLInputElement;
     return element.value;
