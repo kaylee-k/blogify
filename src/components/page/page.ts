d3d85be5cc61eb0680e0684423ae1d3e733a9336
kaylee-k <kaylee.kihm@gmail.com>
2024-09-03 20:19:33 -0600

    Style: Improved drag-and-drop styling

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -20,6 +20,7 @@ interface SectionContainer extends Component, Composable {
   setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
   muteChildren(state: 'mute' | 'unmute'): void;
   getBoundingRect(): DOMRect;
+  onDropped(): void;
 }
 
 export class PageItemComponent
@@ -61,18 +62,26 @@ export class PageItemComponent
 
   onDragStart(_: DragEvent) {
     this.notifyDragObservers('start');
+    this.element.classList.add('lifted');
   }
 
   onDragEnd(_: DragEvent) {
     this.notifyDragObservers('stop');
+    this.element.classList.remove('lifted');
   }
 
   onDragEnter(_: DragEvent) {
     this.notifyDragObservers('enter');
+    this.element.classList.add('drop-area');
   }
 
   onDragLeave(_: DragEvent) {
     this.notifyDragObservers('leave');
+    this.element.classList.remove('drop-area');
+  }
+
+  onDropped() {
+    this.element.classList.remove('drop-area');
   }
 
   notifyDragObservers(state: DragState) {
@@ -145,6 +154,7 @@ export class PageComponent
         dropY < srcElement.y ? 'beforebegin' : 'afterend'
       );
     }
+    this.dropTarget.onDropped();
   }
 
   addChild(section: Component) {
